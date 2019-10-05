872093cae22394a123342d1c191ac222b9264b79
avpatel98 <ayushivpatel@gmail.com>
2019-10-04 20:33:00 -0600

    Added brief comments

--- dragonshell.c
+++ dragonshell.c
@@ -37,6 +37,11 @@ void tokenize(char *str, const char *delim, char **argv)
     }
 }
 
+/**
+ * @brief Forward signals to child processes
+ * 
+ * @param signum - signal number
+ */
 void signal_forward_handler(int signum)
 {
 	if (background_cpid > 0)
@@ -49,18 +54,27 @@ void signal_forward_handler(int signum)
 	}
 }
 
+/**
+ * @brief Identifies if the command is to be run in the background and strips
+ * '&' character from the end if found
+ * 
+ * @param cmd_str - pointer to command string
+ * @return background command flag
+ */
 int is_background_command(char *cmd_str)
 {
 	size_t cmd_len = strlen(cmd_str);
 	
 	while (cmd_len > 0)
 	{
+		// Move past whitespace at the end
 		if ((cmd_str[cmd_len - 1] == ' ') || (cmd_str[cmd_len - 1] == '\t'))
 		{
 			cmd_len--;
 		}
 		else
 		{
+			// Terminate string at '&' character
 			if (cmd_str[cmd_len - 1] == '&')
 			{
 				cmd_str[cmd_len - 1] = '\0';
@@ -73,6 +87,13 @@ int is_background_command(char *cmd_str)
 	return 0;
 }
 
+/**
+ * @brief Searches command for specified character
+ * 
+ * @param cmd_str - pointer to command string
+ * @param chr - character to search for
+ * @return character exists flag
+ */
 int search_command(char *cmd_str, int chr)
 {
 	if (strchr(cmd_str, chr) == NULL)
@@ -82,6 +103,11 @@ int search_command(char *cmd_str, int chr)
 	return 1;
 }
 
+/**
+ * @brief Changes directory
+ * 
+ * @param cmd_param - array pointing to tokenized strings
+ */
 void execute_cd_command(char **cmd_param)
 {
 	if (cmd_param[1] == NULL)
@@ -94,7 +120,10 @@ void execute_cd_command(char **cmd_param)
 	}
 }
 
-void execute_pwd_command(char **cmd_param)
+/**
+ * @brief Prints current working directory
+ */
+void execute_pwd_command(void)
 {
 	char curr_direct[PATH_MAX_SIZE];
 	
@@ -108,6 +137,9 @@ void execute_pwd_command(char **cmd_param)
 	}
 }
 
+/**
+ * @brief Prints PATH
+ */
 void execute_print_path_command(void)
 {
 	printf("Current PATH: ");
@@ -122,6 +154,11 @@ void execute_print_path_command(void)
 	printf("\n");
 }
 
+/**
+ * @brief Appends to or overwites PATH
+ * 
+ * @param cmd_param - array pointing to tokenized strings
+ */
 void execute_a2path_command(char **cmd_param)
 {
 	if (cmd_param[1] == NULL)
@@ -138,10 +175,12 @@ void execute_a2path_command(char **cmd_param)
 		{
 			if (strcmp(direct_paths[0], "$PATH") == 0)
 			{
+				// Append
 				direct_ind = 1;
 			}
 			else
 			{
+				// Overwrite
 				curr_path_num = 0;
 			}
 		}
@@ -153,6 +192,9 @@ void execute_a2path_command(char **cmd_param)
 	}
 }
 
+/**
+ * @brief Kills background process, cleans up zombie processes and exits shell
+ */
 void execute_exit_command(void)
 {
 	printf("Exiting\n");
@@ -167,6 +209,11 @@ void execute_exit_command(void)
 	_exit(0);
 }
 
+/**
+ * @brief Opens file to write output to
+ * 
+ * @param cmd_str - pointer to command string
+ */
 void initiate_redirection(char *cmd_str)
 {
 	char *redirect_param[5] = {NULL};
@@ -189,18 +236,26 @@ void initiate_redirection(char *cmd_str)
 	}
 }
 
+/**
+ * @brief Executes program if it is found within PATH, current working directory; 
+ * or in full path specified by user
+ * 
+ * @param cmd_param - array pointing to tokenized strings
+ */
 void execute_command(char **cmd_param)
 {
 	char *env[] = {NULL};
 	
 	if (strncmp(cmd_param[0], "/", 1) == 0)
 	{
+		// Full path passed in
 		execve(cmd_param[0], cmd_param, env);
 	}
 	else
 	{
 		char direct_path[PATH_MAX_SIZE];
 		
+		// Current working directory
 		if (getcwd(direct_path, PATH_MAX_SIZE) != NULL)
 		{		
 			strcat(direct_path, "/");
@@ -208,6 +263,7 @@ void execute_command(char **cmd_param)
 			execve(direct_path, cmd_param, env);
 		}
 		
+		// PATH
 		for (size_t direct_ind = 0; direct_ind < curr_path_num; direct_ind++)
 		{	
 			strcpy(direct_path, curr_path[direct_ind]);
@@ -218,30 +274,30 @@ void execute_command(char **cmd_param)
 }
 
 int main(int argc, char **argv)
-{
-    // print the string prompt without a newline, before beginning to read
-    // tokenize the input, run the command(s), and print the result
-    // do this in a loop
-    
+{  
     char input_str[INPUT_MAX_SIZE];
     
+    // Register signal handlers
     signal(SIGINT, signal_forward_handler);
-    signal(SIGTSTP, signal_forward_handler);
+    signal(SIGTSTP, signal_forward_handler);   
     
-    printf("Welcome to Dragon Shell!\n");
-    
+    // Initialize path
     strcpy(curr_path[0], "/bin/");
     strcpy(curr_path[1], "/usr/bin/");
     curr_path_num = 2;
     
+    printf("Welcome to Dragon Shell!\n");
+    
     while (1)
     {
-        printf("dragonshell > ");
+        // Print prompt
+    	printf("dragonshell > ");
         fflush(stdout);
         
         if (fgets(input_str, INPUT_MAX_SIZE, stdin) != NULL) 
         {             
-            size_t input_len = strlen(input_str);
+            // Strip newline character or continue if empty command
+        	size_t input_len = strlen(input_str);
             if (input_len > 0)
             {
                 if (input_str[input_len - 1] == '\n')
@@ -257,19 +313,21 @@ int main(int argc, char **argv)
                 }
             }
         	
+            // Get list of commands
             char *cmd_list[TOKEN_MAX_NUM] = {NULL};
         	tokenize(input_str, ";", cmd_list);
         	
+        	// Execute commands in order
         	size_t cmd_ind = 0;
             while (cmd_list[cmd_ind] != NULL)
             {
 				char *cmd_str = cmd_list[cmd_ind++];
 					
+				// Set background and pipe flags
 				int background_flag = is_background_command(cmd_str);
 				int pipe_flag = search_command(cmd_str, '|');
 				
-				char *cmd_param[TOKEN_MAX_NUM] = {NULL};
-				
+				// Find first command in pipe command
 				char *pipe_param[5] = {NULL};
 				if (pipe_flag)
 				{
@@ -277,23 +335,26 @@ int main(int argc, char **argv)
 					cmd_str = pipe_param[0];
 				}
 				
+				// Set output redirection flag and open file if set
 				int redirect_flag = search_command(cmd_str, '>');
 				int stdout_desc = dup(STDOUT_FILENO);
-				
 				if (redirect_flag)
 				{
 					initiate_redirection(cmd_str);
 				}
 				
+				// Obtain command parameters as tokens
+				char *cmd_param[TOKEN_MAX_NUM] = {NULL};
 				tokenize(cmd_str, " \t", cmd_param);
 				
+				// Check through built-in commands
 				if (strcmp(cmd_param[0], "cd") == 0)
 				{
 					execute_cd_command(cmd_param);
 				}
 				else if (strcmp(cmd_param[0], "pwd") == 0)
 				{
-					execute_pwd_command(cmd_param);
+					execute_pwd_command();
 				}
 				else if (strcmp(cmd_param[0], "$PATH") == 0)
 				{
@@ -309,6 +370,7 @@ int main(int argc, char **argv)
 				}
 				else
 				{
+					// Fork for external programs only
 					pid_t pid = fork();
 					if (pid == -1)
 					{
@@ -316,15 +378,18 @@ int main(int argc, char **argv)
 					}
 					else if (pid == 0)
 					{
+						// Reset signal handlers
 						signal(SIGINT, SIG_DFL);
 						signal(SIGTSTP, SIG_DFL);
 						
+						// Close STDOUT and STDERR for background processes
 						if (background_flag)
 						{
 							close(STDOUT_FILENO);
 							close(STDERR_FILENO);
 						}
 						
+						// Open pipe if pipe command
 						if (pipe_flag)
 						{
 							int file_descs[2];
@@ -340,16 +405,20 @@ int main(int argc, char **argv)
 							}
 							else if (cpid == 0)
 							{
+								// Close read
 								close(file_descs[0]);
 								
+								// Only send data over pipe if output redirection is not enabled
 								if (!redirect_flag)
 								{
 									dup2(file_descs[1], STDOUT_FILENO);
 									close(file_descs[1]);
 								}
 								
+								// Execute first command
 								execute_command(cmd_param);
 								
+								// Restore stdout on failure if output redirection is not enabled 
 								if (!redirect_flag)
 								{
 									fflush(stdout);
@@ -359,32 +428,41 @@ int main(int argc, char **argv)
 							}
 							else
 							{
+								// Close write
 								close(file_descs[1]);
+								
+								// Receive data over pipe
 								dup2(file_descs[0], STDIN_FILENO);
 								close(file_descs[0]);
 								
+								// Process second command
 								if (pipe_param[1] != NULL)
 								{
 									cmd_str = pipe_param[1];
 									
+									// Set output redirection flag and open file if set
 									redirect_flag = search_command(cmd_str, '>');
 									if (redirect_flag)
 									{
 										initiate_redirection(cmd_str);
 									}
 									
+									// Obtain command parameters as tokens
 									char *cmd2_param[TOKEN_MAX_NUM] = {NULL};
 									tokenize(cmd_str, " \t", cmd2_param);
 									
+									// Execute second command
 									execute_command(cmd2_param);
 								}
 							}
 						}
 						else
 						{
+							// Execute regular command
 							execute_command(cmd_param);
 						}
 						
+						// Print error message on failure
 						printf("dragonshell: %s: command not found\n", cmd_param[0]);
 						
 						_exit(1);
@@ -393,11 +471,13 @@ int main(int argc, char **argv)
 					{
 						if (background_flag)
 						{
+							// Do not wait on background processes
 							background_cpid = pid;
 							printf("PID %d is running in the background\n", background_cpid);
 						}
 						else
 						{
+							// Wait for current child process to finish
 							foreground_cpid = pid;
 							waitpid(foreground_cpid, NULL, 0);
 							foreground_cpid = -1;
@@ -405,6 +485,7 @@ int main(int argc, char **argv)
 					}
 				}
 				
+				// Reset stdout if output redirection was enabled
 				if (redirect_flag)
 				{
 					fflush(stdout);
@@ -415,6 +496,7 @@ int main(int argc, char **argv)
         }
         else
         {
+        	// Exit on Ctrl-D
         	printf("\n");
         	execute_exit_command();
         }
